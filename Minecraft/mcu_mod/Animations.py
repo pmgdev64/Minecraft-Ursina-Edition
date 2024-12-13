cc57a696a353226649b7d5e9e0b28a3f46fe81b3
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2024-12-13 14:33:54 +0700

    Update Animations.py

--- Minecraft/mcu_mod/Animations.py
+++ Minecraft/mcu_mod/Animations.py
@@ -1,1 +1,10 @@
+from ursina import *
 
+class Animations(self, Objects, Durations):
+  def animate(self, EaseInOut, x, y):
+    self.objects = Objects
+    self.duration = Duration
+    self.easeinout = EaseInOut
+    self.x = x
+    self.y = y
+    
