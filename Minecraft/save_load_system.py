0a5f98eb65209d6ec073281be0873ea88ccd0057
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2024-12-14 15:27:53 +0700

    Update save_load_system.py

--- Minecraft/save_load_system.py
+++ Minecraft/save_load_system.py
@@ -22,46 +22,47 @@ def saveMap(_subPos, _td):
         map_data.clear()
 
 def loadMap(_subject,_terrain):
-    # Open main module directory for correct file.
-    path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    os.chdir(path)
-    with open(mapName, 'rb') as f:
-        map_data = pickle.load(f)
+    if os.path.isfile(mapName):
+        # Open main module directory for correct file.
+        path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        os.chdir(path)
+        with open(mapName, 'rb') as f:
+            map_data = pickle.load(f)
 
-    loadmap=Text(text='loaded map name: '+map_data, scale=0.5)
-    destroy(loadmap, 6) #delete the text after 6sec when the world is loaded (Looks Like savemap).             
+        loadmap=Text(text='loaded map name: '+map_data, scale=0.5)
+        destroy(loadmap, 6) #delete the text after 6sec when the world is loaded (Looks Like savemap).             
                  
 
-    # Empty out current terrain objects.
-    for s in _terrain.subsets:
-        destroy(s)
-    _terrain.td={}
-    _terrain.vd={}
-    _terrain.subsets=[]
-    _terrain.setup_subsets()
-    _terrain.currentSubset=1
-    # Without copy?
-    _terrain.td=map_data[1]
-    # Iterate over terrain dictionary and
-    # if we find 't' then generate a block.
-    # Note this means we'll lose colour info etc.
-    i = 0 # Which subset to build block on?
-    for key in _terrain.td:
-        whatT=_terrain.td.get(key)
-        if whatT!=None and whatT!='g':
-            x = key[0]
-            y = key[1]
-            z = key[2]
-            if i>=len(_terrain.subsets)-1:
-                i=0
-            _terrain.genBlock(x,y,z,subset=i,gap=False,blockType=whatT)
-            i+=1
+        # Empty out current terrain objects.
+        for s in _terrain.subsets:
+            destroy(s)
+        _terrain.td={}
+        _terrain.vd={}
+        _terrain.subsets=[]
+        _terrain.setup_subsets()
+        _terrain.currentSubset=1
+        # Without copy?
+        _terrain.td=map_data[1]
+        # Iterate over terrain dictionary and
+        # if we find 't' then generate a block.
+        # Note this means we'll lose colour info etc.
+        i = 0 # Which subset to build block on?
+        for key in _terrain.td:
+            whatT=_terrain.td.get(key)
+            if whatT!=None and whatT!='g':
+                x = key[0]
+                y = key[1]
+                z = key[2]
+                if i>=len(_terrain.subsets)-1:
+                    i=0
+                _terrain.genBlock(x,y,z,subset=i,gap=False,blockType=whatT)
+                i+=1
 
-    # And reposition subject according to saved map.
-    _subject.position=map_data[0]
-    # Reset swirl engine.
-    _terrain.swirlEngine.reset( _subject.position.x,
+        # And reposition subject according to saved map.
+        _subject.position=map_data[0]
+        # Reset swirl engine.
+        _terrain.swirlEngine.reset( _subject.position.x,
                                 _subject.position.z)
-    # Regenerate subset models, so that we can see terrain.
-    for s in _terrain.subsets:
-        s.model.generate()
+        # Regenerate subset models, so that we can see terrain.
+        for s in _terrain.subsets:
+            s.model.generate()
